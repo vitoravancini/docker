49593c7dae86a82f380b7e3025e2d2293770aa41
vitor <vitorpavancini@gmail.com>
2018-07-01 11:09:03 -0300

    add floripa project

---
+++ projects/floripa/run.sh
@@ -0,0 +1,13 @@
+# configure environment
+sed -i '/DATA_DIR/d' .env
+echo 'DATA_DIR=/data' >> .env
+
+# run build
+pelias compose pull
+pelias elastic start
+pelias elastic wait
+pelias elastic create
+pelias download all
+pelias prepare all
+pelias import all
+pelias compose up
